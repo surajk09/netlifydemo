b48dfeb1ac81b763bde359cf55965f5d76ecbf70
surajk09 <suraj.khadekar@dev.figmd.com>
2019-02-01 18:25:30 +0530

    new files added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Main from "./Main"
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-           Welcome to live website
-          </p>
-          <a
-            className="App-link"
-            href="https://www.htmlrun.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn HTML
-          </a>
+        <div>
+                <Main/>
+            </div>
         </header>
       </div>
     );
---
+++ src/Stuff.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Stuff extends React.Component
+{
+    render(){
+        return(
+            <div>
+                <h1>Stuff</h1>
+            </div>
+        )
+    }
+}
+
+export default Stuff
