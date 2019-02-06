80ddc567e92a737ebf9ce0e21775e224a64ebc17
surajk09 <suraj.khadekar@dev.figmd.com>
2019-02-06 17:08:22 +0530

    new changes and images

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import styled from "styled-components";
 import { BrowserRouter, Route ,Link} from "react-router-dom";
 import Home from "./screens/Home";
-import Details from "./screens/Details";
-import Users from "./screens/Users";
+import Teams from "./screens/Teams";
+import AddConferenceRoom from "./screens/AddConferenceRoom";
 import Posts from "./screens/Posts";
 import {Image ,Menu} from "semantic-ui-react";
-
+import Background from "./images/back1.jpg"
 class App extends React.Component {
   state = {
   }
@@ -16,43 +16,40 @@ class App extends React.Component {
   render(){
     const { activeItem } = this.state
   return (
-    <div>
+    <div  className="login-form"
+  >
+    
     <Menu stackable>
     <Menu.Item>
           <h2>Mark</h2>
         </Menu.Item>
         <Menu.Item
-          name='ConferenceRoom'
-          active={activeItem === 'ConferenceRoom'}
-          onClick={this.handleItemClick}>
+          name='ConferenceRoom'>
         <Link to="/home">
           Home
         </Link></Menu.Item>
-
 <Menu.Item
           name='ConferenceRoom'
-          active={activeItem === 'ConferenceRoom'}
-          onClick={this.handleItemClick}
         >
-    <Link to="/users">
+    <Link to="/AddConferenceRoom">
           Conference Room
         </Link>  </Menu.Item>
 
-        <Menu.Item name='reviews' active={activeItem === 'reviews'} onClick={this.handleItemClick}>
+        <Menu.Item name='reviews'>
+<Link to="/Teams">
           Teams
+          </Link>
         </Menu.Item>
 
         <Menu.Item
           name='upcomingEvents'
-          active={activeItem === 'upcomingEvents'}
-          onClick={this.handleItemClick}
         >
           Upcoming Events
         </Menu.Item>
   </Menu>
       <Route path="/home" component={Home} exact />
-      <Route path="/details" component={Details} exact />
-      <Route path="/users" component={Users} exact />
+      <Route path="/Teams" component={Teams} exact />
+      <Route path="/AddConferenceRoom" component={AddConferenceRoom} exact />
       <Route path="/posts/:userId" component={Posts} />
     </div>
   );
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,69 @@
 import React, { Component } from "react"
 import { Link } from "react-router-dom"
-import {Image} from "semantic-ui-react"
+import {Image,Card,Button,Grid, Divider} from "semantic-ui-react"
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <Image src={require("../images/conferenceRoom.jpg")} size="massive"></Image>
+        <h2>Home</h2>
+<Grid columns={3} stackable>
+<Grid.Row>
+<Grid.Column width={1}></Grid.Column>
+  <Grid.Column width={14} textAlign={"center"}>
+        <Card.Group>
+    <Card>
+      <Card.Content>
+        <Image floated='right' src={require('../images/conferenceRoom.png')} size="tiny" />
+        <Card.Header>Total Conference Rooms</Card.Header>
+       <Divider/>
+        <Card.Header extra> 
+          15
+        </Card.Header>
+      </Card.Content>
+      
+    </Card>
+    <Card>
+      <Card.Content>
+        <Image floated='right' src={require('../images/conferenceRoom.png')} size="tiny" />
+        <Card.Header>Available Conference Rooms</Card.Header>
+        
+        <Divider/>
+        <Card.Header extra> 
+          15
+        </Card.Header>
+      </Card.Content>
+      
+    </Card>
+    <Card>
+      <Card.Content>
+        <Image floated='right' src={require('../images/conferenceRoomBooked.png')} size="tiny" />
+        <Card.Header>Booked Conference Rooms</Card.Header>
+       
+        <Divider/>
+        <Card.Header extra> 
+          15
+        </Card.Header>
+      </Card.Content>
+      
+    </Card>
+    <Card>
+      <Card.Content>
+        <Image floated='right' src={require('../images/projectIcon.png')} size="tiny" />
+        <Card.Header>Total Projects Available</Card.Header>
+        <Divider/>
+        <Card.Header extra> 
+          15
+        </Card.Header>
+      </Card.Content>
+     
+    </Card>
+  </Card.Group>
+  </Grid.Column>
+
+<Grid.Column width={1}></Grid.Column>
+</Grid.Row>
+</Grid>
       </div>
     )
   }
